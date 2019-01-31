822796bcab119c1bdfe2d91d5db1049e24d7082a
Johnley Delgado <johnley00@gmail.com>
2019-01-31 20:54:30 +0800

    Ley

---
+++ LoginSample/ViewController/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  LoginSample
+//
+//  Created by Johnley Delgado on 01/02/2019.
+//  Copyright © 2019 Johnley Delgado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+  
+
+
+}
+
